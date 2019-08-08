2a62d7e0487486607c2dff529a91334bf1d716ff
Jose Cerqueira <jose@origensmedia.com>
2019-08-08 06:47:10 +0100

    Added a new class to refactor the logic to read CSV file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv").read();
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === "H") manUnitedWins++;
   if (match[2] === "Man United" && match[5] === "A") manUnitedWins++;
 }
